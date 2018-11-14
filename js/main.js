1e698bd56ce8d534b5cca85df643b6763e1beb6b
klax83 <kylewins83@gmail.com>
2018-11-14 10:59:55 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -31,33 +31,68 @@ new Item('thing17', 'medium', 'https://pbs.twimg.com/profile_images/507251035929
 new Item('thing18', 'medium', 'https://pbs.twimg.com/profile_images/507251035929190400/BDUL3Uzt_400x400.png', 'white', 140);
 new Item('thing19', 'medium', 'https://pbs.twimg.com/profile_images/507251035929190400/BDUL3Uzt_400x400.png', 'white', 140);
 
+function displayImg() {
+	console.log(this);
+
+	document.getElementById('display').innerHTML = '';
+	
+	var dispImg = document.createElement('div');
+	var image = document.createElement('img');
+// 	// var infoDiv = document.createElement('div');
+	
+	this.children[0].children[1].style.display = 'block';
+	this.children[0].children[0].style.display = 'none';
+	image.src = this.children[0].children[0].src;
+	image.className = 'pt-4 img-fluid';
+	dispImg.className = 'text-center p-auto ';
+	
+	
+	dispImg.appendChild(image);
+	document.getElementById('display').appendChild(dispImg);
+
+}
+
+function deleteImg() {
+	document.getElementById('display').innerHTML = '';
+	this.children[0].children[0].style.display = 'block';
+	this.children[0].children[1].style.display = 'none';
+}
+
 for(var i = 0; i < items.length; i++) {
 	var div = document.createElement('div');
 	var li = document.createElement('a');
-	var h1 = document.createElement('h1');
+	var name = document.createElement('p');
 	var img = document.createElement('img');
-	var a = document.createElement('p');
-	var b = document.createElement('p');
-	var c = document.createElement('p');
-	var butt = document.createElement('button');
-
-	div.className = 'col-lg-3 col-md-6 p-0 z'; 
-	h1.textContent = items[i].name;
+	var size = document.createElement('p');
+	var color = document.createElement('p');
+	var stock = document.createElement('p');
+	var infoDiv = document.createElement('div');
+	
+	div.className = 'col-lg-3 col-md-4 col-sm-6 col-6 p-0 z'; 
+	div.id = i;
+	name.textContent = items[i].name;
 	img.src = items[i].img;
 	img.alt = items[i].name;
-	img.className = 'w-100 img-fluid cover height'
-	a.textContent = items[i].size;
-	b.textContent = items[i].color;
-	c.textContent - items[i].inStock;
-	butt.textContent = 'More Info';
-
-	// div.appendChild(h1);
+	img.className = 'w-100 img-fluid cover height';
+	div.addEventListener('mouseover', displayImg);
+	div.addEventListener('mouseout', deleteImg);
+	size.textContent = "Size: " + items[i].size;
+	size.className = 'pt-2'
+	color.textContent = "Colors: " + items[i].color;
+	stock.textContent = "In stock: " + items[i].inStock;
+	infoDiv.className = 'text-center font-weight-bold ';
+	infoDiv.id = 'infoDiv';
+	infoDiv.style.display = 'none';
+	infoDiv.style.height = infoDiv.clientWidth;
+	
+	
 	li.appendChild(img);
 	div.appendChild(li);
-	// div.appendChild(a);
-	// div.appendChild(b);
-	// div.appendChild(c);
-	// div.appendChild(butt);
+	// infoDiv.appendChild(name);
+	infoDiv.appendChild(size);
+	infoDiv.appendChild(color);
+	infoDiv.appendChild(stock);
+	li.appendChild(infoDiv);
 
 	document.getElementById('target').appendChild(div);
 
